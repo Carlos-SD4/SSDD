2b8ad1a735e6cf98118a15bd86ba0df70966a740
Carlos-SD4 <146247295+Carlos-SD4@users.noreply.github.com>
2023-12-06 09:57:31 +0100

    Update app.py

--- icedrive_directory/app.py
+++ icedrive_directory/app.py
@@ -6,29 +6,28 @@ from typing import List
 
 import Ice
 
-from .directory import Directory
-
+from .directory import Directory, DirectoryService
 
 class DirectoryApp(Ice.Application):
-    """Implementation of the Ice.Application for the Authentication service."""
+    """Implementation of the Ice.Application for the Directory service."""
 
     def run(self, args: List[str]) -> int:
-        """Execute the code for the AuthentacionApp class."""
-        adapter = self.communicator().createObjectAdapter("DirectoryAdapter")
-        adapter.activate()
+        """Execute the code for the DirectoryApp class."""
+        adapter = self.communicator().createObjectAdapter("DirectoryAdapter") 
 
-        servant = Directory()
-        servant_proxy = adapter.addWithUUID(servant)
+        directory_service_impl = DirectoryService()
 
-        logging.info("Proxy: %s", servant_proxy)
+        directory_service_proxy = adapter.addWithUUID(directory_service_impl)
+        adapter.activate() 
+
+        logging.info("DirectoryService Proxy: %s", directory_service_proxy)
 
         self.shutdownOnInterrupt()
         self.communicator().waitForShutdown()
 
         return 0
 
-
 def main():
-    """Handle the icedrive-authentication program."""
+    """Handle the icedrive-directory program."""
     app = DirectoryApp()
     return app.main(sys.argv)
