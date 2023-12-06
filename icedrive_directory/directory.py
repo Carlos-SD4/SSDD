6fae31f71b370bccff6d8e4171ffcf0a59fcf9b9
Carlos-SD4 <146247295+Carlos-SD4@users.noreply.github.com>
2023-12-06 10:15:55 +0100

    Update directory.py

--- icedrive_directory/directory.py
+++ icedrive_directory/directory.py
@@ -9,9 +9,19 @@ import IceDrive
 
 class Directory(IceDrive.Directory):
     """Implementation of the IceDrive.Directory interface."""
+        def __init__(self, name: str, user_name: str):
+        self.user_name = user_name
+        self.name = name
+        self.parent = None
+        self.childs = {}
+        self.files = {}
 
     def getParent(self, current: Ice.Current = None) -> IceDrive.DirectoryPrx:
         """Return the proxy to the parent directory, if it exists. None in other case."""
+        if self.parent is None:
+            return None
+        else:
+            return IceDrive.DirectoryPrx.uncheckedCast(current.adapter.addWithUUID(self.parent))
 
     def getChilds(self, current: Ice.Current = None) -> List[str]:
         """Return a list of names of the directories contained in the directory."""
