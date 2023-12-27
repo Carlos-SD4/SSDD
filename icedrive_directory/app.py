a3515ec32a176fbcfeb64a587431eb8f6693a0a3
Carlos Sánchez Díaz <carlos.sanchez74@alu.uclm.es>
2023-12-27 19:20:30 +0100

    Cambios instancia Discovery

--- icedrive_directory/app.py
+++ icedrive_directory/app.py
@@ -11,8 +11,6 @@ import threading
 import time
 
 class DirectoryApp(Ice.Application):
-    """Implementation of the Ice.Application for the Directory service."""
-
     def get_topic_manager(self):
         key = 'IceStorm.TopicManager.Proxy'
         proxy = self.communicator().propertyToProxy(key)
@@ -29,56 +27,52 @@ class DirectoryApp(Ice.Application):
             time.sleep(interval)
 
     def run(self, args: List[str]) -> int:
-        """Execute the code for the DirectoryApp class."""
         topic_mgr = self.get_topic_manager()
         if not topic_mgr:
             print('Invalid proxy')
             return 2
+
         topic_name = "Discovery"
         try:
             topic = topic_mgr.retrieve(topic_name)
         except IceStorm.NoSuchTopic:
             topic = topic_mgr.create(topic_name)
         publicador = topic.getPublisher()
-        discovery = Discovery()
-        discovery_prx = adapter.addWithUUID(discovery)
-
-        qos ={}
-        publicador.subscribeAndGetPublisher(qos, discovery_prx)
 
         directory_topic_name = "DirectoryService"
         try:
             directory_topic = topic_mgr.retrieve(directory_topic_name)
         except IceStorm.NoSuchTopic:
             directory_topic = topic_mgr.create(directory_topic_name)
-        directory_topic.subscribeAndGetPublisher(qos, discovery_prx)
-
 
         authentication_topic_name = "Authentication"
         try:
             authentication_topic = topic_mgr.retrieve(authentication_topic_name)
         except IceStorm.NoSuchTopic:
             authentication_topic = topic_mgr.create(authentication_topic_name)
-        authentication_topic.subscribeAndGetPublisher(qos, discovery_prx)
-
 
         blob_topic_name = "BlobService"
         try:
             blob_topic = topic_mgr.retrieve(blob_topic_name)
         except IceStorm.NoSuchTopic:
             blob_topic = topic_mgr.create(blob_topic_name)
-        blob_topic.subscribeAndGetPublisher(qos, discovery_prx)
-
-
 
         adapter = self.communicator().createObjectAdapter("DirectoryAdapter")
 
         directory_service_impl = DirectoryService()
-
         directory_service_proxy = adapter.addWithUUID(directory_service_impl)
         adapter.activate()
 
         logging.info("DirectoryService Proxy: %s", directory_service_proxy)
+
+        discovery = Discovery(directory_service_proxy)
+        discovery_prx = adapter.addWithUUID(discovery)
+        qos = {}
+        publicador.subscribeAndGetPublisher(qos, discovery_prx)
+
+        directory_topic.subscribeAndGetPublisher(qos, discovery_prx)
+        authentication_topic.subscribeAndGetPublisher(qos, discovery_prx)
+        blob_topic.subscribeAndGetPublisher(qos, discovery_prx)
 
         publish_thread = threading.Thread(
             target=self.publish_directory_service_periodically,
@@ -97,6 +91,6 @@ class DirectoryApp(Ice.Application):
         return 0
 
 def main():
-    """Handle the icedrive-directory program."""
     app = DirectoryApp()
     return app.main(sys.argv)
+
--- icedrive_directory/discovery.py
+++ icedrive_directory/discovery.py
@@ -8,30 +8,27 @@ import IceDrive
 class Discovery(IceDrive.Discovery):
     """Servants class for service discovery."""
 
-    def __init__(self):
-
+    def __init__(self,my_prx):
+        self.my_prx = my_prx
         self.authentication_proxies = []
         self.directory_service_proxies = []
         self.blob_service_proxies = []
 
     def announceAuthentication(self, prx: IceDrive.AuthenticationPrx, current: Ice.Current = None) -> None:
         """Receive an Authentication service announcement."""
-
-        self.authentication_proxies.append(prx)
-        print(f"Received Authentication announcement: {prx.ice_toString()}")
+        if prx not in self.authentication_proxies:
+            self.authentication_proxies.append(prx)
+            print(f"Received Authentication announcement: {prx.ice_toString()}")
 
     def announceDirectoryServicey(self, prx: IceDrive.DirectoryServicePrx, current: Ice.Current = None) -> None:
         """Receive a Directory service announcement."""
-        '''Mandar mi propio DirectoryServicePrx'''
 
-
-        '''Controlar que no reciba mensajes mios'''
-        if prx not in self.directory_service_proxies:
+        if prx not in self.directory_service_proxies and prx != self.my_prx:
             self.directory_service_proxies.append(prx)
             print(f"Received DirectoryService announcement: {prx.ice_toString()}")
 
     def announceBlobService(self, prx: IceDrive.BlobServicePrx, current: Ice.Current = None) -> None:
         """Receive a Blob service announcement."""
-
-        self.blob_service_proxies.append(prx)
-        print(f"Received BlobService announcement: {prx.ice_toString()}")
+        if prx not in self.blob_service_proxies:
+            self.blob_service_proxies.append(prx)
+            print(f"Received BlobService announcement: {prx.ice_toString()}")
